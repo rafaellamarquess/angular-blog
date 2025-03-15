8506c97331fee37c8d1316fc8f54318d1b7119e6
Rafaella <fariassrafaella@gmail.com>
2025-03-15 18:35:06 -0300

    corrigindo title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { RouterModule } from '@angular/router';
 import { Component } from '@angular/core';
 import { MenuBarComponent } from "./components/menu-bar/menu-bar.component";
-import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
-
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [RouterModule, MenuBarComponent, MenuTitleComponent]
+  imports: [RouterModule, MenuBarComponent]
 })
 
 export class AppComponent {
